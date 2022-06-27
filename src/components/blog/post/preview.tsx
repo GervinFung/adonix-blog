4bfcf6b2ba0288d30e282cc2c3eb015c888dfd59
GervinFung <gervinfungdaxuen@gmail.com>
2022-06-27 14:43:12 +0800

    feat(minutes-to-read): added minutes-to-read

--- src/components/blog/post/preview.tsx
+++ src/components/blog/post/preview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import minutesToRead from 'minutes-to-read';
 import StringToMarkdown from './parser';
 import { NonNullablePublishedPost } from '../../../common/type/post';
 
@@ -26,31 +27,41 @@ const Preview = ({
         />
         <Typography
             variant="h3"
-            component="div"
+            component="h3"
             sx={{
-                mb: 1,
+                mb: 2,
             }}
         >
             {title}
         </Typography>
         <Typography
-            variant="subtitle2"
+            variant="h6"
             color="text.secondary"
-            component="div"
+            component="h4"
             sx={{
-                mb: 4,
+                mb: 2,
             }}
         >
             {timePublished.toDateString()}
         </Typography>
         <Typography
             variant="h5"
-            component="div"
+            component="h5"
             sx={{
                 mb: 2,
             }}
         >
             {description}
+        </Typography>
+        <Typography
+            variant="h6"
+            color="text.secondary"
+            component="em"
+            sx={{
+                mb: 1,
+            }}
+        >
+            {minutesToRead(content)}
         </Typography>
         <Typography
             variant="subtitle1"
